d257a7ba5bc4fa6f903350899514cbf85e710e83
Maria <mpfc.maria@gmail.com>
2021-10-19 19:36:34 -0300

    aula 01 - fixing migrations and setting up websocket

--- node_heat/src/app.ts
+++ node_heat/src/app.ts
@@ -1,12 +1,28 @@
 import "dotenv/config";
 import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import cors from "cors";
 
 import { router } from "./routes";
 
 const app = express();
 
-app.use(express.json());
+app.use(cors())
 
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuario conectado no socket ${socket.id}`);
+})
+
+
+app.use(express.json());
 app.use(router);
 
 
@@ -19,4 +35,5 @@ app.get("/signin/callback", (request, response)=> {
     return response.json(code);
 })
 
-app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000`));
+
+export {serverHttp, io};
--- node_heat/src/controllers/CreateMessageController.ts
+++ node_heat/src/controllers/CreateMessageController.ts
@@ -10,6 +10,7 @@ class CreateMessageController {
        const service = new CreateMessageService();
        const result = await service.execute(message, user_id);
 
+
        return response.json(result);
 
    } 
